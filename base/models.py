5c82b34d3b3cba112c8413bbeca9f5447467180a
pkkulhari <praveen.kulhari07@gmail.com>
2022-01-13 18:53:50 +0530

    fix: default user avatar image

--- base/models.py
+++ base/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=100, null=True)
     email = models.EmailField(unique=True)
     bio = models.TextField(null=True)
-    avatar = models.ImageField(default='avatar.svg')
+    avatar = models.ImageField()
 
 
 class Topic(models.Model):
@@ -23,12 +23,12 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    participants = models.ManyToManyField(User, related_name='participants')
+    participants = models.ManyToManyField(User, related_name="participants")
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created', '-updated']
+        ordering = ["-created", "-updated"]
 
     def __str__(self):
         return self.name
